6105f89c91543b04ee030314cb779b2c6a8cb993
KforKelvin <ky1015@tamu.edu>
2023-01-28 15:00:18 -0600

    update table

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -7,45 +7,33 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
 
 export const Tables = (props) => {
 
   const rows = [
-    { id: 1, name: 'John Doe', age: 42 },
-    { id: 2, name: 'Jane Smith', age: 34 },
-    { id: 3, name: 'Bob Johnson', age: 27 },
+    { id: 1, company: "American Express", name: 'Blue Cash Everyday Card (BCE)', fee: 42, purpose: "Grocery", offer: 200, grocery:3 , gas:3, online:3, dining: 1, other: 0   },
+    { id: 1, company: "American Express", name: 'Blue Cash Everyday Card (BCE)', fee: 42, purpose: "Grocery", offer: 200, grocery:3 , gas:3, online:3, dining: 1, other: 0   },
+    { id: 1, company: "American Express", name: 'Blue Cash Everyday Card (BCE)', fee: 42, purpose: "Grocery", offer: 200, grocery:3 , gas:3, online:3, dining: 1, other: 0   },
   ];
 
   return (
       <TableContainer component={Paper}>
+       
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat&nbsp;(g)</TableCell>
-              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell align="right">Company</TableCell>
+              <TableCell align="right">Fee</TableCell>
+              <TableCell align="right">Offer</TableCell>
+              <TableCell align="right">Cash back on Grocery&nbsp;(%)</TableCell>
+              <TableCell align="right">Cash back on Gas&nbsp;(%)</TableCell>
+              <TableCell align="right">Cash back on online retail&nbsp;(%)</TableCell>
+              <TableCell align="right">Cash back on Dining&nbsp;(%)</TableCell>
+              <TableCell align="right">Cash back on Others&nbsp;(%)</TableCell>
             </TableRow>
           </TableHead>
+          
           <TableBody>
             {rows.map((row) => (
               <TableRow
@@ -55,14 +43,19 @@ export const Tables = (props) => {
                 <TableCell component="th" scope="row">
                   {row.name}
                 </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
+                <TableCell align="right">{row.company}</TableCell>
+                <TableCell align="right">{row.fee}</TableCell>
+                <TableCell align="right">{row.offer}</TableCell>
+                <TableCell align="right">{row.grocery}</TableCell>
+                <TableCell align="right">{row.gas}</TableCell>
+                <TableCell align="right">{row.online}</TableCell>
+                <TableCell align="right">{row.dining}</TableCell>
+                <TableCell align="right">{row.other}</TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
+      
       </TableContainer>
     );
 };
