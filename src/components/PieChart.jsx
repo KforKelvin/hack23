1bd7b01ccc132db96afb933ba56fd31bbad45e2a
EndlessDebugger <dortizchaves@outlook.com>
2023-01-28 22:13:53 -0600

    added cash_back pie based on inputs

--- src/components/PieChart.jsx
+++ src/components/PieChart.jsx
@@ -14,8 +14,13 @@ export default class PieChart extends React.Component {
                         type: 'pie'
                        },
                   layout: { datarevision: 0, legend: {"orientation": "h"},margin: {l:0, r:0, b:0, t:0},hieght: 300,width: 300,},
-                  revision: 0
-                ,card:" "};
+                  revision: 0,
+                  card:" ",
+                  cash_back_pie: {
+                    values: [100,100,100,100,100],
+                    labels: ["Grocery","Dinning","Gas", "Retail", "Others"], 
+                    type: 'pie'
+                  }};
     
     this.handleGroceryChange = this.handleGroceryChange.bind(this);
     this.handleDinningChange = this.handleDinningChange.bind(this);
@@ -41,14 +46,6 @@ export default class PieChart extends React.Component {
     this.setState({others: event.target.value});
   }
 
-  renderResult(val) {
-    return (
-      <div>
-        <h2>Recommended Card: { val}</h2>
-      </div>
-    );
-  }
-
   handleSubmit(event) {
     
     event.preventDefault();
@@ -62,7 +59,7 @@ export default class PieChart extends React.Component {
     const current = { values: vals,
                       labels: ["Grocery","Dinning","Gas", "Retail", "Others"], 
                       type: 'pie'
-                    }
+                    };
 
     this.setState({ pie: current });
     this.setState({ revision: this.state.revision + 1 });
@@ -72,16 +69,40 @@ export default class PieChart extends React.Component {
                               legend: {"orientation": "h"},
                               margin: {l:0, r:0, b:0, t:0},} });
   
-    let benefitsArr = new Array(this.props.data[0].length);
+    let benefit_len = this.props.data[0].length;
+    let benefitsArr = new Array(benefit_len);
 
-    for (let i = 0; i < this.props.data[0].length; i++) {
-      let {offer, fee, grocery, gas, online, dining, other} = this.props.data[0][i];
-      let benefit = offer/12 - fee/12 + u_grocery * grocery * 0.01 + u_gas * gas * 0.01 + u_retail * online * 0.01 + u_dinning * dining * 0.01 + u_others * other * 0.01;
+    for (let i = 0; i < benefit_len; i++) {
+      let {offer, fee, grocery, gas, online, dinning, other} = this.props.data[0][i];
+      console.log(other);
+      let benefit = offer/12 - fee/12 
+                    + u_grocery * grocery * 0.01 
+                    + u_gas * gas * 0.01 
+                    + u_retail * online * 0.01 
+                    + u_dinning * dinning * 0.01 
+                    + u_others * other * 0.01;
       benefitsArr[i] = benefit;
+      console.log(benefit);
     }
+
+    console.log(benefitsArr);
     let max_benefit_idx = benefitsArr.indexOf(Math.max(...benefitsArr));
 
-    this.setState({ card:this.props.data[0][max_benefit_idx].name});    
+    this.setState({card : this.props.data[0][max_benefit_idx].name}); 
+    alert(this.state.card);
+    let {offer, fee, grocery, gas, online, dinning, other} = this.props.data[0][max_benefit_idx];
+    let cash_grocery = u_grocery * grocery * 0.01;
+    let cash_gas= u_gas * gas * 0.01;
+    let cash_retail = u_retail * online * 0.01;
+    let cash_dinning = u_dinning * dinning * 0.01;
+    let cash_others = u_others * other * 0.01;
+    
+    let cash_back_vals = [cash_grocery, cash_gas, cash_retail, cash_dinning, cash_others ];
+    const cash__back_current = { values: cash_back_vals,
+      labels: ["Grocery","Dinning","Gas", "Retail", "Others"], 
+      type: 'pie'
+    }
+    this.setState({ cash_back_pie: cash__back_current });
   }
 
    
@@ -90,75 +111,78 @@ export default class PieChart extends React.Component {
     
     return (
       <div id="cards" className="text-center">
-      <div className="container">
-        <div className="col-md-10 col-md-offset-1 section-title">
-          <h1></h1>
-          <h2>Credit Card Recommendation</h2>
+        <div className="container">
+          <div className="col-md-10 col-md-offset-1 section-title">
+            <h1></h1>
+            <h2>Credit Card Recommendation</h2>
+          </div>
         </div>
-        </div>
+        
         <div className='text-center' >
           <div className='pie-row'>
-          <div>
-            <h3>My Budget Distribution: </h3>
+            <div>
+              <h3>My Budget Distribution: </h3>
               <div className='piechart'>
-                  <Plot 
+                <Plot 
+                  data={[
+                    this.state.pie,
+                  ]}
+                  layout={this.state.layout}
+                  revision={this.state.revision}
+                  graphDiv="graph"
+                />
+              </div>
+            </div>
+
+            <div className='form-col'>
+              <h3>Enter My Monthly Budget:</h3>
+                
+              <form onSubmit={this.handleSubmit}>
+                <label>
+                  Grocery: 
+                  <input type="number" name="grocery" defaultValue={this.state.grocery} onChange={this.handleGroceryChange}/>
+                </label>
+
+                <label>
+                  Dinning: 
+                  <input type="number" name="dinning" defaultValue={this.state.dinning} onChange={this.handleDinningChange}/>
+                </label>
+
+                <label>
+                  Gas: 
+                  <input type="number" name="gas" defaultValue={this.state.gas} onChange={this.handleGasChange}/>
+                </label>
+
+                <label>
+                  Online Retail: 
+                  <input type="number" name="retail" defaultValue={this.state.retail} onChange={this.handleRetailChange}/>
+                </label>
+
+                <label>
+                  Others: 
+                  <input type="number" name="Others" defaultValue={this.state.others} onChange={this.handleOthersChange}/>
+                </label>
+                <input type="submit" value="Submit"/>
+              </form>
+
+            </div>
+          </div>
+        </div>
+
+        {this.state.card === " " ? <p> </p> :
+            <div>
+                <h2>Recommended Card: { this.state.card}</h2>
+
+                <Plot 
                     data={[
-                      this.state.pie,
+                      this.state.cash_back_pie,
                     ]}
                     layout={this.state.layout}
                     revision={this.state.revision}
-                    graphDiv="graph"
-                  />
+                    />
               </div>
-
-          </div>
-          <div className='form-col'>
-            <h3>Enter My Monthly Budget:</h3>
-            
-            <form onSubmit={this.handleSubmit}>
-              <label>
-                Grocery: 
-                <input type="number" name="grocery" defaultValue={this.state.grocery} onChange={this.handleGroceryChange}/>
-              </label>
-
-              <label>
-                Dinning: 
-                <input type="number" name="dinning" defaultValue={this.state.dinning} onChange={this.handleDinningChange}/>
-              </label>
-
-              <label>
-                Gas: 
-                <input type="number" name="gas" defaultValue={this.state.gas} onChange={this.handleGasChange}/>
-              </label>
-
-              <label>
-                Online Retail: 
-                <input type="number" name="retail" defaultValue={this.state.retail} onChange={this.handleRetailChange}/>
-              </label>
-
-              <label>
-                Others: 
-                <input type="number" name="Others" defaultValue={this.state.others} onChange={this.handleOthersChange}/>
-              </label>
-
-              <input type="submit" value="Submit"/>
-            </form>
-            
-
-            
-
-          </div>
-          </div>
-
-          {this.state.card === " " ? <p> </p> :
-              <div>
-                <h2>Recommended Card: { this.state.card}</h2>
-              </div>
-            }
-        </div>
-
-        </div>
-
+        }
+    </div>
     );
 
   }
